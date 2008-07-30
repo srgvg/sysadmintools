62af530c317fcb3a79e562252fb5951b5c09580b
svg <svg@a33c6f8c-bf51-0410-aefe-ad234f6e076f>
2008-07-30 13:54:57 +0000

    dump more than 1 host in 1 command
    git-svn-id: https://support.ginsys.be/svn/scripts@8 a33c6f8c-bf51-0410-aefe-ad234f6e076f

--- vmware/vcbBackup.sh
+++ vmware/vcbBackup.sh
@@ -2,7 +2,9 @@
 
 read -p "Enter username to connect to virtcenter: " USER && echo
 read -s -p "Enter password for user $USER: " PASS && echo
-vcbVmName -h virtcenter -u $USER -p $PASS -s any | grep name:
-read -p "Enter the name of the host to be backed up: " NAME && echo
-echo vcbMounter -h virtcenter -u $USER -p $PASS  -a name:$NAME -r /mnt/backup/$NAME-$(date +%Y%m%d) -t fullvm -m cos
+vcbVmName -h virtcenter -u $USER -p $PASS -s any | grep name: || exit
+read -p "Enter the names of the hosts to be backed up, separated by a space: " NAME && echo
+for HOST in $(echo $NAME)
+        do vcbMounter -h virtcenter -u $USER -p $PASS  -a name:$HOST -r /mnt/backup/$HOST-$(date +%Y%m%d) -t fullvm -m cos
+        done
 
