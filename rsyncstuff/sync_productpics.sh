c1b31e3b97c1939da9e54ce9bb5a629fc9513100
svg <svg@a33c6f8c-bf51-0410-aefe-ad234f6e076f>
2008-08-06 11:57:21 +0000

    import sync_productpics.sh
    git-svn-id: https://support.ginsys.be/svn/scripts@13 a33c6f8c-bf51-0410-aefe-ad234f6e076f

---
+++ rsyncstuff/sync_productpics.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+src_host="hades2"
+src_dir="/mnt/disk2/ScratchDTP/jpg/"
+dst_dir="/mnt/rootusa/Resources/Departments/Shared Files/Pictures/Products/"
+prod_list="vi_data.txt"
+
+rm "$dst_dir/$prod_list"
+rsync "$src_host:$src_dir/$prod_list" "$dst_dir" || (echo error retrieving product list; exit)
+
+sed -i s@^@"+ "@ "$dst_dir/$prod_list"
+echo "+ */" >> "$dst_dir/$prod_list"
+echo "- *" >> "$dst_dir/$prod_list"
+
+rsync -az --exclude-from="$dst_dir/$prod_list" "$src_host:$src_dir/" "$dst_dir" 
+
