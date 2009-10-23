86659de490a0ab2d00fe61f554a9698563b3827d
svg <svg@a33c6f8c-bf51-0410-aefe-ad234f6e076f>
2009-10-23 15:33:39 +0000

    git-svn-id: https://support.ginsys.be/svn/scripts@67 a33c6f8c-bf51-0410-aefe-ad234f6e076f

--- virtest/hostest.sh
+++ virtest/hostest.sh
@@ -8,197 +8,48 @@
 
 ################################################################################
 # parameters
+################################################################################
 
 KERNEL=linux-2.6.31.tar.bz2
 KERNELCONF=config-2.6.31-14-server
-WAVES=" DeadWhiteGuys1.wav  mrsandman.wav  starthal.wav  vivaldi.wav "
 TESTDIR=/var/lib/libvirt/images/bonnietest
 TESTUSER=atlantis
 DBENCHNUMCLIENT=10
+IPERFSERVER=virtestserver.wall.test
+IPERFNUMBYTES=1024M
+IPERFNUMCLITHRD=2
 
 ################################################################################
 # initialization
+################################################################################
 
+VERBOSE=0
+INIT=0
+JNIT=0
 MYIP=$( ip -f inet route show dev eth0 | grep src | awk '{ print $7 }' )
 BASEDIR=$(cd $(dirname $0); pwd)
 FILESDIR=$BASEDIR/files
+FILESWAVDIR=$FILESDIR/wav
 WORKDIR=$BASEDIR/work
 KERNELDIR=$WORKDIR/kernel
 LAMEDIR=$WORKDIR/lame
-LOGDIR=$LOGBASEDIR/$TIMESTAMP
 LOGBASEDIR=$WORKDIR/logs/$(hostname)-${MYIP}
-TIMESTAMP=$(date +%Y%m%d%H%M)
 
-uname -a | grep x86_64 >/dev/null && mbandwidth="./bandwith64" || mbandwidth="./bandwith32"
-################################################################################
-init_env() {
-cd $BASEDIR
-mkdir -p $LOGDIR $KERNELDIR $LAMEDIR $TESTDIR
-chmod 777 $TESTDIR
-[ -L $LOGBASEDIR/latest ] && rm $LOGBASEDIR/latest || ln -s $TIMESTAMP $LOGBASEDIR/latest
-
-# install dependencies
-	[ -x "$(which ssh)" ] 		|| packages=$packages" openssh-client"
-	[ -x "$(which bonnie++)" ] 	|| packages=$packages" bonnie++"
-	[ -x "$(which iperf)" ] 	|| packages=$packages" iperf"
-	[ -x "$(which lame)" ] 		|| packages=$packages" lame"
-	[ -x "$(which bzip2)" ] 	|| packages=$packages" bzip2"
-	[ -x "$(which lshw)" ] 		|| packages=$packages" lshw"
-	[ -x "$(which dmidecode)" ] 	|| packages=$packages" dmidecode"
-	[ -x "$(which gcc)" ] 		|| packages=$packages" gcc"
-	[ -x "$(which dbench)" ] 	|| packages=$packages" dbench"
-	[ -x "$(which netperf)" ] 	|| packages=$packages" netperf"
-#	[ -x  ] 			|| packages=$packages" "
-if [ ! -z "$packages" ]
-then #install packages
-	if [ -x $(which aptitude) ]
-	then #debian or ubuntu
-		aptitude update && aptitude upgrade 
-		aptitude install $packages
-	elif [ -x $( which yum ) ]
-	then #centos or fedora
-		yum update
-		echo "Please check for missing packages:"
-		yum install $packages
-		echo "Please check for missing packages!"
-	else
-		echo Failed to detect distribution, please make sure \
-		necessary packages are installed. 
-		exit 1
-	fi
-fi
-}
-################################################################################
-init_srv_env() {
-cd $BASEDIR
-mkdir -p $LOGDIR 
-
-# install dependencies
-	[ -x "$(which iperf)" ] 	|| packages=$packages" iperf"
-	[ -x "$(which dbench)" ] 	|| packages=$packages" dbench"
-	[ -x "$(which netperf)" ] 	|| packages=$packages" netperf"
-#	[ -x  ] 			|| packages=$packages" "
-if [ ! -z "$packages" ]
-then #install packages
-	if [ -x $(which aptitude) ]
-	then #debian or ubuntu
-		aptitude update && aptitude upgrade 
-		aptitude install $packages
-	elif [ -x $( which yum ) ]
-	then #centos or fedora
-		yum update
-		echo "Please check for missing packages:"
-		yum install $packages
-		echo "Please check for missing packages!"
-	else
-		echo Failed to detect distribution, please make sure \
-		necessary packages are installed. 
-		exit 1
-	fi
-fi
-}
-
-# Userspace - bzip
-test_zip() {
-time bunzip2 -v -c $FILESDIR/$KERNEL       >$KERNELDIR/${KERNEL%.*} 2>/dev/null  2>$LOGDIR/bunzip2.log
-time bzip2   -v -c $KERNELDIR/${KERNEL%.bz2} >$KERNELDIR/$KERNEL      2>/dev/null  2>$LOGDIR/bzip2.log
-time tar -xv -f $KERNELDIR/${KERNEL%.bz2}    -C $KERNELDIR   	 2>/dev/null  2>$LOGDIR/untar.log
-}
-
-# Userspace kernel compile kernel compile 
-test_kernelcompile() {
-cd $KERNELDIR/${KERNEL%.tar.bz2}
-make clean
-cp $FILESDIR/$KERNELCONF ./.config
-time make         | tee /dev/null >/dev/null 2>&1  >$LOGDIR/kernelmake.log
-time make modules | tee /dev/null >/dev/null 2>&1 >>$LOGDIR/kernelmake.log
-cd $BASEDIR
-}
-
-# lame
-test_lame() {
-for wave in $(cd $FILESDIR; ls *.wav)
-do
-	time lame $FILESDIR/$wave $LAMEDIR/${wave%.wav}.mp3 >$LOGDIR/${wave%.wav}-lame.log 2>/dev/null 2>$LOGDIR/${wave%.wav}-lametime.log
-done
-}
-
-# unixbench
-test_unixbench() {
-cd $BASEDIR/unixbench
-make clean
-make | tee $LOGDIR/unixbench.log
-#./Run index arithmetic system misc dhry | tee $LOGDIR/unixbench.log
-make run | tee -a $LOGDIR/unixbench.log
-make report | tee $LOGDIR/unixbench-report.log
-cd $BASEDIR
-}
-
-# bonnie++
-test_bonnie() {
-bonnie -d TESTDIR -u $TESTUSER | tee $LOGDIR/bonnie.log
-}
-
-# iozone 
-test_iozone() {
-ozone -a | tee $LOGDIR/iozone.log
-}
-
-# dbench
-test_dbench() {
-dbench -D $TESTDIR $DBENCHNUMCLIENT | tee $LOGDIR/dbench.log
-}
-
-# iperf 
-test_iperf_server() {
-iperf
-}
-test_iperf() {
-iperf
-}
-
-# tbench 
-test_tbench_server() {
-tbench -D $TESTDIR $DBENCHNUMCLIENT | tee $LOGDIR/dbench.log
-}
-test_tbench() {
-tbench -D $TESTDIR $DBENCHNUMCLIENT | tee $LOGDIR/dbench.log
-}
-
-# Memory bandwidth test
-test_mbandwidth() {
-$mbandwidth 2>$LOGDIR/memory-bandwidth.log
-}
-
-# hardware info
-test_hwinfo() {
-lshw -short -C memory >$LOGDIR/memory-info.txt
-dmidecode --type 17 >>$LOGDIR/memory-info.txt
-}
-
-# sysbench cpu
-test_sysbench_cpu() {
-sysbench --test=cpu  --cpu-max-prime=100000 run >$LOGDIR/sysbench-cpu.log
-}
-
-
-
-################################################################################
-# MAIN
-
-INIT=0
 available_tests=$(grep test_.*\(\) $0 | sed -e 's/^.*test_//' | sort | cut -d\( -f1 | tr '\n' ' ' )
+
 usage_information() {
-printf "
-Initializing and executing perofmance tests.
+	printf " 
+`basename $0`     Initializing and executing perofmance tests.
 
 Usage: $(basename $0) [-h] [-i] [-t test1,test2...]
    -h	shows this help
+   -v	verbose
    -i	initialize the environment
+   -T	timestamp or test identifier, defaults to `date +%Y%m%d`
    -t	Available tests are: 
 	$available_tests
 
-This script must be run as root.
+This script expects to be run as root.
 \n"  >&2
 }
 
@@ -208,22 +59,22 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1
 fi
 
-# parse command line
+# parse command line options
 if [ $# -eq 0 ]; then #  must be at least one arg
         usage_information >&2
         exit 1
 fi
 
-while getopts 'dhijt:' OPTION
+while getopts 'dvijT:t:h' OPTION
 do
 	case $OPTION in
 	d)	set -x ;;
-	i)	printf "\nInitializing environment....\n"
-		init_env && exit 0 || exit 2
+	v)	VERBOSE=1;;
+	i)	INIT=1
 		;;
-	j)	printf "\nInitializing server environment....\n"
-		init_srv_env && exit 0 || exit 2
+	j)	JNIT=1
 		;;
+	T)	TIMESTAMP=${OPTARG// /_}};;
 	t)	TESTS=$(echo "$OPTARG" | tr ',' ' ')
 		invalid_tests=""
 		all_tests_valid=0
@@ -238,7 +89,7 @@ do
 			if [ $requested_test_valid = 0 ]
 				then 	invalid_tests=$invalid_tests" "$requested_test
 			fi
-			if [ ! -z $invalid_tests ]
+			if [ ! -z "$invalid_tests" ]
 			then	echo You requested invalid tests: $invalid_tests.
 				usage_information >&2
 				exit 1
@@ -258,10 +109,211 @@ do
 	esac
 done
 
+[ -z $TIMESTAMP ] && TIMESTAMP=$(date +%Y%m%d)
+LOGDIR=$LOGBASEDIR/$TIMESTAMP
+cd $BASEDIR
+mkdir -p $LOGDIR $KERNELDIR $LAMEDIR $TESTDIR
+[ -L $LOGBASEDIR/latest ] && rm $LOGBASEDIR/latest || ln -s $TIMESTAMP $LOGBASEDIR/latest
+chmod 777 $TESTDIR
+uname -a | grep x86_64 >/dev/null && mbandwidth="./bandwith64" || mbandwidth="./bandwith32"
 
-for TEST in $TESTS
-do	test_procedure="test_$TEST"
-	echo Executing $test_procedure
-	$test_procedure 
+################################################################################
+# functions
+################################################################################
+
+# set logging
+set_logging() {
+if [ $VERBOSE = 1 ]
+then	 LOG=" | tee    $LOGDIR/${test_procedure}_output.log 2> $LOGDIR/${test_procedure}_error.log 2> $LOGDIR/${test_procedure}_time.log"
+	LOGA=" | tee -a $LOGDIR/${test_procedure}_output.log 2>>$LOGDIR/${test_procedure}_error.log 2>>$LOGDIR/${test_procedure}_time.log"
+	NOLOG=""
+else	 LOG=" >        $LOGDIR/${test_procedure}_output.log 2> $LOGDIR/${test_procedure}_error.log 2> $LOGDIR/${test_procedure}_time.log"
+	LOGA=" >>       $LOGDIR/${test_procedure}_output.log 2>>$LOGDIR/${test_procedure}_error.log 2>>$LOGDIR/${test_procedure}_time.log"
+	NOLOG=">/dev/null 2>&1"
+fi
+}
+
+init_env() {
+	# install dependencies
+		[ -x "$(which ssh)" ] 		|| packages=$packages" openssh-client"
+		[ -x "$(which bonnie++)" ] 	|| packages=$packages" bonnie++"
+		[ -x "$(which iperf)" ] 	|| packages=$packages" iperf"
+		[ -x "$(which lame)" ] 		|| packages=$packages" lame"
+		[ -x "$(which bzip2)" ] 	|| packages=$packages" bzip2"
+		[ -x "$(which lshw)" ] 		|| packages=$packages" lshw"
+		[ -x "$(which dmidecode)" ] 	|| packages=$packages" dmidecode"
+		[ -x "$(which gcc)" ] 		|| packages=$packages" gcc"
+		[ -x "$(which dbench)" ] 	|| packages=$packages" dbench"
+		[ -x "$(which netperf)" ] 	|| packages=$packages" netperf"
+	#	[ -x  ] 			|| packages=$packages" "
+	if [ ! -z "$packages" ]
+	then #install packages
+		if [ -x $(which aptitude) ]
+		then #debian or ubuntu
+			aptitude update && aptitude upgrade 
+			aptitude install $packages
+		elif [ -x $( which yum ) ]
+		then #centos or fedora
+			yum update
+			echo "Please check for missing packages:"
+			yum install $packages
+			echo "Please check for missing packages!"
+		else
+			echo Failed to detect distribution, please make sure \
+			necessary packages are installed. 
+			exit 1
+		fi
+	fi
+}
+
+init_srv_env() {
+	cd $BASEDIR
+	mkdir -p $LOGDIR 
+	
+	# install dependencies
+		[ -x "$(which iperf)" ] 	|| packages=$packages" iperf"
+		[ -x "$(which dbench)" ] 	|| packages=$packages" dbench"
+		[ -x "$(which netperf)" ] 	|| packages=$packages" netperf"
+	#	[ -x  ] 			|| packages=$packages" "
+	if [ ! -z "$packages" ]
+	then #install packages
+		if [ -x $(which aptitude) ]
+		then #debian or ubuntu
+			aptitude update && aptitude upgrade 
+			aptitude install $packages
+		elif [ -x $( which yum ) ]
+		then #centos or fedora
+			yum update
+			echo "Please check for missing packages:"
+			yum install $packages
+			echo "Please check for missing packages!"
+		else
+			echo Failed to detect distribution, please make sure \
+			necessary packages are installed. 
+			exit 1
+		fi
+	fi
+}
+
+################################################################################
+#TESTS
+################################################################################
+
+test_zip() {
+	eval time bunzip2 -v -c $FILESDIR/$KERNEL         >  $KERNELDIR/${KERNEL%.*} $LOG
+	eval time bzip2   -v -c $KERNELDIR/${KERNEL%.bz2} >  $KERNELDIR/$KERNEL      $LOG
+	eval time tar    -xv -f $KERNELDIR/${KERNEL%.bz2} -C $KERNELDIR              $LOG
+}
+
+test_compression() {
+break #this doesn't work yet
+plot="set terminal postscript, set output compression.ps, "
+for compr in bzip2 lzma gzip
+do	echo -n>$a
+	for blocksize in $(seq 0 256)
+	do 	dd if=/dev/zero of=$blocksize.zero bs=$blocksize count=1
+		start=$(date +%s%N)
+		$compr $b.zero
+		end=$(date +%s%N)
+		total=$(echo $end-$start|bc)
+		echo $total>>$compr
+		rm $blocksize.*
+	done
+	plot=${plot}", plot'"$compr" with lines, "
 done
+plot=$(echo $plot | sed s/\,\ $//)
+echo $plot | gnuplot -persist $LOGDIR/${test_procedure}/compression.png
+}
+
+test_kernelcompile() {
+	cd $KERNELDIR/${KERNEL%.tar.bz2}
+	eval make clean $NOLOG
+	eval cp -v $FILESDIR/$KERNELCONF ./.config $NOLOG
+	eval time make         $LOG
+	eval time make modules $LOGA
+	cd $BASEDIR
+}
+
+test_lame() {
+	for wave in $(cd $FILESWAVDIR; ls *.wav)
+	do
+		eval time lame $FILESDIR/$wave $LAMEDIR/${wave%.wav}.mp3 $LOG
+	done
+}
+
+test_unixbench() {
+	cd $BASEDIR/unixbench
+	eval make clean $NOLOG
+	eval make $LOG
+	#./Run index arithmetic system misc dhry | tee $LOGDIR/unixbench.log
+	eval make run    $LOGA
+	eval make report $LOGA
+	cd $BASEDIR
+}
+
+test_bonnie() {
+	eval bonnie -d TESTDIR -u $TESTUSER $LOG
+}
+
+test_iozone() {
+	eval ozone -a $LOG
+}
+
+test_dbench() {
+	eval dbench -D $TESTDIR $DBENCHNUMCLIENT $LOG
+}
+
+test_iperf_server() {
+	eval iperf -u -s > $LOGDIR/${test_procedure}_output.log 2> $LOGDIR/${test_procedure}_error.log & 
+	echo $!
+}
+
+test_iperf() {
+	eval iperf -u -c $IPERFSERVER -d -n $IPERFNUMBYTES -P $IPERFNUMCLITHRD  $LOG
+}
+
+test_tbench_server() {
+	eval tbench_srv $LOG > $LOGDIR/${test_procedure}_output.log 2> $LOGDIR/${test_procedure}_error.log & 
+	echo $!
+}
+
+test_tbench() {
+	eval tbench -D $TESTDIR $DBENCHNUMCLIENT $TBENCHSERVER $LOG
+}
+
+test_mbandwidth() {
+	eval $mbandwidth $LOG
+}
+
+test_hwmeminfo() {
+	eval time lshw -short -C memory $LOG
+	eval time dmidecode --type 17   $LOGA
+}
+
+test_sysbench_cpu() {
+	eval time sysbench --test=cpu  --cpu-max-prime=100000 run $LOG
+}
+
+
+################################################################################
+# MAIN
+################################################################################
+
+if [ $INIT = 1 ]
+then	printf "\nInitializing server environment....\n"
+	init_srv_env && exit 0 || exit 2
+
+elif [ $JNIT = 1 ]
+then	printf "\nInitializing environment....\n"
+	init_env && exit 0 || exit 2
+
+elif [ ! -z "$TESTS" ]
+then	for TEST in $TESTS
+	do	test_procedure="test_$TEST"
+		echo Executing $test_procedure
+		set_logging
+		eval $test_procedure
+	done
+fi
+
 
--- virtest/upload.sh
+++ virtest/upload.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+[ $# = 0 ] && { echo need target host; exit; }
 host=$1
 shift
 rsync -avL --delete $(dirname $0) root@$host:~/virtest/ --exclude=.swp $*
